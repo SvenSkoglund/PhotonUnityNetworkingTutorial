521b68c4298c4a41574fc6b490c48aa7c186d971
Sven Skoglund <32058317+SvenSkoglund@users.noreply.github.com>
2020-03-08 13:52:29 -0600

    Step 7 setup player networking

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,7 +2,7 @@
 using UnityEngine.EventSystems;
 
 using Photon.Pun;
-
+using Photon.Pun.Demo.PunBasics;
 using System.Collections;
 
 namespace Com.MyCompany.MyGame
@@ -11,8 +11,32 @@ namespace Com.MyCompany.MyGame
     /// Player manager.
     /// Handles fire Input and Beams.
     /// </summary>
-    public class PlayerManager : MonoBehaviourPunCallbacks
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     {
+        #region IPunObservable implementation
+
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(IsFiring);
+                stream.SendNext(Health);
+
+            }
+            else
+            {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+
+            }
+        }
+
+
+        #endregion
+
         #region Private Fields
 
         [Tooltip("The Beams GameObject to control")]
@@ -79,13 +103,35 @@ namespace Com.MyCompany.MyGame
         }
 
         /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+
+
+            if (_cameraWork != null)
+            {
+                if (photonView.IsMine)
+                {
+                    _cameraWork.OnStartFollowing();
+                }
+            }
+            else
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            }
+        }
+
+        /// <summary>
         /// MonoBehaviour method called on GameObject by Unity on every frame.
         /// </summary>
         void Update()
         {
-
-            ProcessInputs();
-
+            if (photonView.IsMine)
+            {
+                ProcessInputs();
+            }
             // trigger Beams active state
             if (beams != null && IsFiring != beams.activeInHierarchy)
             {
